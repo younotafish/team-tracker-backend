29bd489dff5087be0e8074d21cd9b118a41e1205
= <=>
2020-08-07 18:03:07 -0700

    jpa

--- src/main/java/com/teamtracker/backend/repository/ProjectTaskRepository.java
+++ src/main/java/com/teamtracker/backend/repository/ProjectTaskRepository.java
@@ -1,9 +1,9 @@
 package com.teamtracker.backend.repository;
 
 import com.teamtracker.backend.domain.ProjectTask;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectTaskRepository extends CrudRepository<ProjectTask,Long> {
+public interface ProjectTaskRepository extends JpaRepository<ProjectTask,Long> {
   ProjectTask findByProjectNameAndOwnerNameAndTaskName(String projectName, String ownerName,String taskName);
   Iterable<ProjectTask> findByProjectNameAndOwnerName(String projectName, String ownerName);
   Iterable<ProjectTask> findByProjectNameAndOwnerNameAndStatus(String projectName, String ownerName, String status);
